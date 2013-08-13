eab03ffd509d728f6bc9cc461da953300d213fca
Jonathan Lim <snowblink@gmail.com>
2013-08-13 20:25:37 +0100

    Pass Bob test 3

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -2,6 +2,8 @@ var Bob = function(){
   this.hey = function(noise){
     if (noise.toUpperCase() == noise) {
       return "Woah, chill out!";
+    } else if (noise.match(/\?$/)) {
+      return "Sure.";
     } else {
       return "Whatever.";
     }
