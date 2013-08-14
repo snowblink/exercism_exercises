a0bfd8eac6d7d3511532b2c3f21d2457a5612554
Jonathan Lim <jonlim@notonthehighstreet.com>
2013-08-14 14:29:11 +0100

    Refactor Noise out into own class

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,13 +1,27 @@
 var Bob = function(){
-  this.hey = function(noise){
-    if (noise.toUpperCase() == noise) {
-      return "Woah, chill out!";
-    } else if (noise.match(/\?$/)) {
-      return "Sure.";
-    } else {
-      return "Whatever.";
+    this.hey = function(noise){
+        var noise = new Noise(noise);
+
+        if (noise.shouting()){
+            return "Woah, chill out!";
+        } else if (noise.question()) {
+            return "Sure.";
+        } else {
+            return "Whatever.";
+        }
     }
-  }
+}
+
+var Noise = function(noise){
+    var noise = noise;
+
+    this.shouting = function(){
+        return (noise.toUpperCase() == noise);
+    }
+
+    this.question = function(){
+        return (noise.match(/\?$/));
+    }
 }
 
 module.exports = Bob;
