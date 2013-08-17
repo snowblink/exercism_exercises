6663592168c493f068adddb7b9b54d0aba57024a
Jonathan Lim <snowblink@gmail.com>
2013-08-17 18:20:55 +0100

    Pass word count test 1-3

--- javascript/word-count/word-count_test.spec.js
+++ javascript/word-count/word-count_test.spec.js
@@ -8,19 +8,19 @@ describe("Words", function() {
     expect(words.count).toEqual(expectedCounts);
   });
 
-  xit("counts one of each", function() {
+  it("counts one of each", function() {
     var words = new Words("one of each");
     var expectedCounts = { "one": 1, "of": 1, "each": 1 };
     expect(words.count).toEqual(expectedCounts);
   });
 
-  xit("counts multiple occurrences", function() {
+  it("counts multiple occurrences", function() {
     var words = new Words("one fish two fish red fish blue fish");
     var expectedCounts = { "one" : 1, "fish" : 4, "two" : 1, "red" : 1, "blue" : 1 };
     expect(words.count).toEqual(expectedCounts);
   });
 
-  xit("ignores punctuation", function() {
+  it("ignores punctuation", function() {
     var words = new Words("car : carpet as java : javascript!!&@$%^&");
     var expectedCounts = { "car" : 1, "carpet" : 1, "as" : 1, "java" : 1, "javascript" : 1 };
     expect(words.count).toEqual(expectedCounts);
