e5f00a02e8f67e90cafba208efcd230352f79412
Jonathan Lim <jonlim@notonthehighstreet.com>
2013-08-22 14:18:59 +0100

    Use strict and passed through JSHint

--- javascript/word-count/words.js
+++ javascript/word-count/words.js
@@ -1,14 +1,16 @@
 function Words(words) {
-    this.findWords = words.match(/\w+/g);
+    "use strict";
 
-    this.count = this.findWords.reduce(function(memo, current){
+    var findWords = words.match(/\w+/g);
+
+    this.count = findWords.reduce(function(memo, current){
         current = current.toLowerCase();
 
-        memo[current] = memo[current] || 0
+        memo[current] = memo[current] || 0;
         memo[current] += 1;
 
         return memo;
     }, {});
-};
+}
 
 module.exports = Words;
