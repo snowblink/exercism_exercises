2e14dca6ed29e348394fd2ff798bf43f35f1a582
Jonathan Lim <snowblink@gmail.com>
2013-08-24 08:58:23 +0100

    Pass beer song test 1

--- javascript/beer-song/beer.js
+++ javascript/beer-song/beer.js
@@ -3,6 +3,13 @@ function Beer(){
 
 Beer.verse = function(number){
     "use strict";
+
+    var nextNumber = number - 1;
+
+    return number + " bottles of beer on the wall, " +
+    number + " bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    nextNumber + " bottles of beer on the wall.\n";
 };
 
 module.exports = Beer;
