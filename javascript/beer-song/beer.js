f5dfa2e0ca912f3a5e366c1075d5680266a4fe71
Jonathan Lim <snowblink@gmail.com>
2013-08-24 09:55:25 +0100

    Pass beer tests 1-3

--- javascript/beer-song/beer.js
+++ javascript/beer-song/beer.js
@@ -5,6 +5,9 @@ Beer.verse = function(number){
     "use strict";
 
     var nextNumber = number - 1;
+    if (nextNumber < 0){
+        nextNumber = 99;
+    }
 
     var bottlesOfBeer = function(number){
         if (number == 1){
@@ -16,12 +19,31 @@ Beer.verse = function(number){
         }
     };
 
-    return bottlesOfBeer(number) + " on the wall, " +
-    bottlesOfBeer(number) + ".\n" +
-    "Take " +
-    (number === 1 ? "it" : "one") +
-    " down and pass it around, " +
-    bottlesOfBeer(nextNumber) + " on the wall.\n";
+    var onTheWall = bottlesOfBeer(number).capitalize() +
+        " on the wall, " +
+        bottlesOfBeer(number) + ".\n";
+
+        var drinkOne = function(){
+            var result = "";
+            if (nextNumber === 99){
+                result += "Go to the store and buy some more, ";
+            } else {
+            result += "Take " +
+            (number === 1 ? "it" : "one") +
+            " down and pass it around, ";
+            }
+
+            result += bottlesOfBeer(nextNumber) + " on the wall.\n";
+            return result;
+        };
+
+    return onTheWall + drinkOne();
+
+
+};
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
 module.exports = Beer;
