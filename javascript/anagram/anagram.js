6c06f5c9630a9f82ec1e89494e0f9047290697e6
Jonathan Lim <snowblink@gmail.com>
2013-08-27 10:31:45 +0100

    spaces after functions

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,13 +1,13 @@
 function Anagram(subject){
     "use strict";
 
-    this.match = function(possibleAnagrams){
+    this.match = function(possibleAnagrams) {
         return possibleAnagrams.filter(function(currentAnagram){
             return (isAnagramOf(currentAnagram, subject));
         });
     };
 
-    var isAnagramOf = function(a, b){
+    var isAnagramOf = function(a, b) {
         if (a == b){
             return false;
         } else {
@@ -15,7 +15,7 @@ function Anagram(subject){
         }
     };
 
-    var normalizeString = function(string){
+    var normalizeString = function(string) {
         return string.toLowerCase().split('').sort().join('');
     };
 }
