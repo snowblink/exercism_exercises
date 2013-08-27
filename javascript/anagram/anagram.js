6da86f2ff1407db7f59bcbebb491f72a83a3f15b
Jonathan Lim <snowblink@gmail.com>
2013-08-27 11:16:22 +0100

    camelCase the variables and use filter instead of reduce

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,13 +1,10 @@
 function Anagram(subject){
     "use strict";
 
-    this.match = function(possible_anagrams){
-        return possible_anagrams.reduce(function(memo, current_anagram){
-            if (isAnagramOf(current_anagram, subject) === true){
-                memo.push(current_anagram);
-            }
-            return memo;
-        }, []);
+    this.match = function(possibleAnagrams){
+        return possibleAnagrams.filter(function(currentAnagram){
+            return (isAnagramOf(currentAnagram, subject));
+        });
     };
 
     var isAnagramOf = function(a, b){
