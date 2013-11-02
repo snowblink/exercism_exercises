ff863a0d2727710ac0687976009a5de827a04cc0
Jonathan Lim <snowblink@gmail.com>
2013-11-02 10:59:01 +0000

    Pass all hamming tests

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,15 @@
 class Hamming
-  def self.compute(a,b)
-    return 0 if a == b
+  def self.compute(strand_a, strand_b)
+    return 0 if strand_a == strand_b
+    strand_b_as_array = strand_b.chars
+
+    hamming = 0
+    strand_a.chars.each_with_index do |char, i|
+      break if i == strand_a.chars.length || i == strand_b.chars.length
+      if char != strand_b_as_array[i]
+        hamming += 1
+      end
+    end
+    hamming
   end
 end
--- ruby/hamming/hamming_test.rb
+++ ruby/hamming/hamming_test.rb
@@ -17,32 +17,26 @@ class HammingTest < MiniTest::Unit::TestCase
   end
 
   def test_complete_hamming_distance_in_small_strand
-    skip
     assert_equal 3, Hamming.compute('ACT', 'GGA')
   end
 
   def test_hamming_distance_in_off_by_one_strand
-    skip
     assert_equal 9, Hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')
   end
 
   def test_small_hamming_distance_in_middle_somewhere
-    skip
     assert_equal 1, Hamming.compute('GGACG', 'GGTCG')
   end
 
   def test_larger_distance
-    skip
     assert_equal 2, Hamming.compute('ACCAGGG', 'ACTATGG')
   end
 
   def test_ignores_extra_length_on_other_strand_when_longer
-    skip
     assert_equal 3, Hamming.compute('AAACTAGGGG', 'AGGCTAGCGGTAGGAC')
   end
 
   def test_ignores_extra_length_on_original_strand_when_longer
-    skip
     assert_equal 5, Hamming.compute('GACTACGGACAGGGTAGGGAAT', 'GACATCGCACACC')
   end
 end
