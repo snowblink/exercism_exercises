fbfc67a3cffaa000d0e98c8d49546b18a0dd9878
Jonathan Lim <snowblink@gmail.com>
2014-02-18 01:11:49 +0800

    Fix updated leap

--- ruby/leap/leap_test.rb
+++ ruby/leap/leap_test.rb
@@ -7,17 +7,14 @@ class YearTest < MiniTest::Unit::TestCase
   end
 
   def test_non_leap_year
-    skip
     refute Year.leap?(1997)
   end
 
   def test_century
-    skip
     refute Year.leap?(1900)
   end
 
   def test_fourth_century
-    skip
     assert Year.leap?(2400)
   end
 end
