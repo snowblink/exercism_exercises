94ff4381bee272890f64b959a63278ffda3badf3
Jonathan Lim <snowblink@gmail.com>
2014-02-18 08:38:11 +0800

    Another refactor

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,7 +1,14 @@
 class Grains
+  def initialize
+    @_square = Array.new
+  end
+
   def square(i)
     return 1 if i == 1
-    return 2 * square(i-1)
+    if @_square[i].nil?
+      @_square[i] = 2 * square(i-1)
+    end
+    @_square[i]
   end
 
   def total
