c4f8a6cb6ce06638b53e75ad4ce843eb585b55ff
Jonathan Lim <snowblink@gmail.com>
2014-02-24 15:10:54 +0800

    First go at go!

---
+++ go/leap/leap.go
@@ -0,0 +1,21 @@
+package leap
+
+func IsLeapYear(year int) bool {
+  if ( StandardLeapYear(year) && NotCentury(year) ) || CenturyLeapYear(year) {
+    return true
+    } else {
+      return false
+    }
+}
+
+func StandardLeapYear(year int) bool {
+  return year % 4 == 0
+}
+
+func NotCentury(year int) bool {
+  return year % 100 != 0
+}
+
+func CenturyLeapYear(year int) bool {
+  return year % 400 == 0
+}
