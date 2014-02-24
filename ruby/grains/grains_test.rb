76165d29b07b3ede73765d7d81fedeab17e62955
Jonathan Lim <snowblink@gmail.com>
2014-02-24 13:51:17 +0800

    Enjoy a more colourful test suite

--- ruby/grains/grains_test.rb
+++ ruby/grains/grains_test.rb
@@ -1,4 +1,10 @@
+require 'rubygems'
+require 'bundler/setup'
+
 require 'minitest/autorun'
+require "minitest/reporters"
+MiniTest::Reporters.use!
+
 require_relative 'grains'
 
 class GrainsTest < MiniTest::Unit::TestCase
