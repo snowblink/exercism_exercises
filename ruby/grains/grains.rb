1d712c9648f2da31bde9ad3de1c98538c6b91047
Jonathan Lim <snowblink@gmail.com>
2014-07-05 21:07:40 +0100

    Use maths

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,18 +1,11 @@
 class Grains
   TOTAL_NUMBER_OF_SQUARES = 64
-  @@squares = []
 
   def square(i)
-    return 1 if i == 1
-    if @@squares[i].nil?
-      @@squares[i] = 2 * square(i-1)
-    end
-    @@squares[i]
+    (2 ** (i - 1))
   end
 
   def total
-    (1..TOTAL_NUMBER_OF_SQUARES).inject(0) do |memo, i|
-      memo += square(i)
-    end
+    (2 ** TOTAL_NUMBER_OF_SQUARES) - 1
   end
 end
