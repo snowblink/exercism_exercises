4fe4a24a0435975b094fe9f0670c8077c97e2c53
Jonathan Lim <snowblink@gmail.com>
2016-06-05 22:49:21 +0100

    Run through go fmt

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -4,8 +4,8 @@ const testVersion = 2
 
 // determine if given year is a leap year
 func IsLeapYear(year int) bool {
-	div_by_four := year % 4 == 0
-	div_by_hundred := year % 100 == 0
-	div_by_four_hundred := year % 400 == 0
+	div_by_four := year%4 == 0
+	div_by_hundred := year%100 == 0
+	div_by_four_hundred := year%400 == 0
 	return (div_by_four && !div_by_hundred) || div_by_four_hundred
 }
