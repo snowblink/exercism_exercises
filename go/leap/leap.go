7d6d3dd31b7920343edeb05bef57c4ebb0215715
Jonathan Lim <snowblink@gmail.com>
2016-06-05 22:47:48 +0100

    Remove stubbed comments

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,12 +1,8 @@
-// Leap stub file
-
-// The package name is expected by the test program.
 package leap
 
-// testVersion should match the targetTestVersion in the test file.
 const testVersion = 2
 
-// It's good style to write a comment here documenting IsLeapYear.
+// determine if given year is a leap year
 func IsLeapYear(year int) bool {
 	div_by_four := year % 4 == 0
 	div_by_hundred := year % 100 == 0
