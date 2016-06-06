addf42347542f927fa417a9f7049a7d5cbac23b3
Jonathan Lim <snowblink@gmail.com>
2016-06-06 16:56:09 +0100

    Update comment

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,8 +1,9 @@
+// Package leap provides methods to deal with leap years.
 package leap
 
 const testVersion = 2
 
-// determine if given year is a leap year
+// IsLeapYear returns if a year is a leap year or not.
 func IsLeapYear(year int) bool {
 	div_by_four := year%4 == 0
 	div_by_hundred := year%100 == 0
