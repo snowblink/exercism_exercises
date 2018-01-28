5ac98cec0520153179d567f8812af33cc67e1677
Jonathan Lim <snowblink@gmail.com>
2018-01-28 16:52:00 +0000

    Works, but only sometimes.
    Ranging over a map is not guaranteed to retain order

---
+++ go/raindrops/raindrops.go
@@ -0,0 +1,29 @@
+package raindrops
+
+import "strconv"
+
+var factors = map[int]string{
+	3: "Pling",
+	5: "Plang",
+	7: "Plong",
+}
+
+// Convert a number to a string, the contents of which depend on the number's factors.
+//
+// - If the number has 3 as a factor, output 'Pling'.
+// - If the number has 5 as a factor, output 'Plang'.
+// - If the number has 7 as a factor, output 'Plong'.
+// - If the number does not have 3, 5, or 7 as a factor,
+//   just pass the number's digits straight through.
+func Convert(n int) string {
+	var result string
+	for factor, say := range factors {
+		if n%factor == 0 {
+			result += say
+		}
+	}
+	if len(result) == 0 {
+		result = strconv.Itoa(n)
+	}
+	return result
+}
