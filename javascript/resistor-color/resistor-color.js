5d7e337273e8cbf5e094887c204cb308acfe6d81
Jonathan Lim <snowblink@gmail.com>
2019-11-20 12:19:11 +0000

    Add JS resistor-color

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,16 @@
+export const colorCode = (color) => {
+  return COLORS.indexOf(color.toLowerCase())
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
