3c78a70d1e4d601896f0063d74c819f385670381
Jonathan Lim <snowblink@gmail.com>
2019-11-20 10:41:09 +0000

    Add Haskell Space Age

---
+++ haskell/space-age/.stack-work/dist/x86_64-osx/Cabal-2.4.0.1/build/autogen/Paths_space_age.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_space_age (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [1,2,0,6] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/Users/pairingdev/programming/exercism_exercises/haskell/space-age/.stack-work/install/x86_64-osx/750d8e10ba5e79fe0df1290d0163f7be21a732089530f425ce30c8644794d7e2/8.6.5/bin"
+libdir     = "/Users/pairingdev/programming/exercism_exercises/haskell/space-age/.stack-work/install/x86_64-osx/750d8e10ba5e79fe0df1290d0163f7be21a732089530f425ce30c8644794d7e2/8.6.5/lib/x86_64-osx-ghc-8.6.5/space-age-1.2.0.6-ElEfD3s87oPLIe7ZfFbqQO"
+dynlibdir  = "/Users/pairingdev/programming/exercism_exercises/haskell/space-age/.stack-work/install/x86_64-osx/750d8e10ba5e79fe0df1290d0163f7be21a732089530f425ce30c8644794d7e2/8.6.5/lib/x86_64-osx-ghc-8.6.5"
+datadir    = "/Users/pairingdev/programming/exercism_exercises/haskell/space-age/.stack-work/install/x86_64-osx/750d8e10ba5e79fe0df1290d0163f7be21a732089530f425ce30c8644794d7e2/8.6.5/share/x86_64-osx-ghc-8.6.5/space-age-1.2.0.6"
+libexecdir = "/Users/pairingdev/programming/exercism_exercises/haskell/space-age/.stack-work/install/x86_64-osx/750d8e10ba5e79fe0df1290d0163f7be21a732089530f425ce30c8644794d7e2/8.6.5/libexec/x86_64-osx-ghc-8.6.5/space-age-1.2.0.6"
+sysconfdir = "/Users/pairingdev/programming/exercism_exercises/haskell/space-age/.stack-work/install/x86_64-osx/750d8e10ba5e79fe0df1290d0163f7be21a732089530f425ce30c8644794d7e2/8.6.5/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "space_age_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "space_age_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "space_age_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "space_age_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "space_age_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "space_age_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
