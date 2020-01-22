186dd19c982752e48b9c365417b672310534dd56
Jonathan Lim <snowblink@gmail.com>
2020-01-22 13:54:29 +0000

    Try some suggestions

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -4,11 +4,12 @@ import Data.Char
 
 responseFor :: String -> String
 responseFor xs
-  | null (removeWhitespace xs) || null xs = "Fine. Be that way!"
+  | null trimmed = "Fine. Be that way!"
   | not(onlyNumbersOrPunctuation xs == xs) && last xs == '?' && map toUpper xs == xs = "Calm down, I know what I'm doing!"
-  | last (removeWhitespace xs) == '?' = "Sure."
+  | last trimmed == '?' = "Sure."
   | not(onlyNumbers xs == xs) && map toUpper xs == xs = "Whoa, chill out!"
   | otherwise = "Whatever."
   where onlyNumbers ys = [ y | y <- ys, y `elem` "?, " ++ ['0'..'9'] ]
         onlyNumbersOrPunctuation ys = [ y | y <- ys, y `elem` "?,:) " ++ ['0'..'9']]
         removeWhitespace ys = [ y | y <- ys, not(y `elem` " \t\n\r")]
+        trimmed = removeWhitespace xs
