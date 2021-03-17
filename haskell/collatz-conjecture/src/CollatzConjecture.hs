f6b8612fbdf0050406594d67e5822f130fe33f7a
Jonathan Lim <snowblink@gmail.com>
2021-03-17 13:23:48 +0000

    Final conditional unneeded

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -7,4 +7,4 @@ collatz m =
           | n <= 0 = Nothing
           | n == 1 = Just acc
           | even n = tailCollatz (n `div` 2) (1 + acc)
-          | odd n = tailCollatz (n * 3 + 1) (1 + acc)
+          | otherwise = tailCollatz (n * 3 + 1) (1 + acc)
