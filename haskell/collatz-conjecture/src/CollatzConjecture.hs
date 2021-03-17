173a5f612fc562e29ab6445b1ec8a38866f93a32
Jonathan Lim <snowblink@gmail.com>
2021-03-17 12:15:25 +0000

    Tidy up

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,12 +1,10 @@
 module CollatzConjecture (collatz) where
 
-import Data.Maybe (fromJust)
-
 collatz :: Integer -> Maybe Integer
 collatz n =
   tailCollatz n 0
   where tailCollatz n acc
           | n <= 0 = Nothing
-          | n == 1 = Just (0 + acc)
+          | n == 1 = Just acc
           | even n = tailCollatz (n `div` 2) (1 + acc)
           | odd n = tailCollatz (n * 3 + 1) (1 + acc)
