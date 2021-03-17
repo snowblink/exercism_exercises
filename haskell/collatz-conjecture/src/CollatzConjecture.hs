624056a0f41f81e8ae7c228b20b1de024b967750
Jonathan Lim <snowblink@gmail.com>
2021-03-17 09:33:48 +0000

    Remove unnecessary test conditions

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -6,6 +6,6 @@ collatz :: Integer -> Maybe Integer
 collatz n
   | n <= 0 = Nothing
   | n == 1 = Just 0
-  | even n && isJust (collatz (n `div` 2)) = Just (1 + fromJust (collatz (n `div` 2)))
-  | odd n && isJust (collatz (n * 3 + 1)) = Just (1 + fromJust (collatz (n * 3 + 1)))
+  | even n = Just (1 + fromJust (collatz (n `div` 2)))
+  | odd n = Just (1 + fromJust (collatz (n * 3 + 1)))
   | otherwise = Nothing
