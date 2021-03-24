6561b87b4adda96c7d4ce4184f254a09fea9f98c
Jonathan Lim <snowblink@gmail.com>
2021-03-24 10:10:51 +0000

    Move conditional out, so that tailCollatz doesn't need Maybe

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,10 +1,11 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz m =
-  tailCollatz m 0
-  where tailCollatz n acc
-          | n <= 0 = Nothing
-          | n == 1 = Just acc
-          | even n = tailCollatz (n `div` 2) (1 + acc)
-          | otherwise = tailCollatz (n * 3 + 1) (1 + acc)
+collatz m
+  | m <= 0 = Nothing
+  | otherwise = Just (tailCollatz m 0)
+  where
+    tailCollatz n acc
+      | n == 1 = acc
+      | even n = tailCollatz (n `div` 2) (1 + acc)
+      | otherwise = tailCollatz (n * 3 + 1) (1 + acc)
